28eb1591c4ecfd7afbf50c3fd53cd6612926607c
surendrayadavcse <venkatasurendra70@gmail.com>
2024-11-29 00:08:17 +0530

    first commit

--- Back-End/DB_Connection/db.js
+++ Back-End/DB_Connection/db.js
@@ -1,12 +1,17 @@
 const mongoose = require('mongoose');
+require('dotenv').config(); // Ensure dotenv is loaded to access environment variables
 
-(async function connectDb(){
-    try{
-    await mongoose.connect("mongodb+srv://venkatasurendra70:Surendra%40639@ecommerce-cluster.vz5yq.mongodb.net/E-Commerce")
-    console.log('connected db successfully')
-    }
-    catch(err){
-        console.log("some issue while connectiong to db",err)
+const mongouri = process.env.MONGO_URI;
 
-    }
+(async function connectDb() {
+  try {
+    // Options added to handle deprecation warnings
+    await mongoose.connect(mongouri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('Connected to DB successfully');
+  } catch (err) {
+    console.error('Error while connecting to DB:', err);
+  }
 })();
